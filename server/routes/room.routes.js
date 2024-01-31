69a721c5fdebe76cc19c2495b17cad7b3db442cf
Slacky300 <rehman.khan.300a@gmail.com>
2024-01-31 16:31:08 +0530

    kk

--- server/routes/room.routes.js
+++ server/routes/room.routes.js
@@ -1,4 +1,4 @@
-const { createRoom, deleteRoom, updateRoom, getRoomsByProject } = require('../controllers/room.controllers')
+const { createRoom, deleteRoom, updateRoom, getRoomsByProject, getRoomById } = require('../controllers/room.controllers')
 
 const {validateToken} = require('../middleware/validateToken');
 
@@ -8,6 +8,7 @@ const router = require('express').Router();
 
 router.post('/create/:id', validateToken, createRoom);
 router.delete('/delete/:id', validateToken, deleteRoom);
+router.get('/single/:id', validateToken, getRoomById);
 router.put('/update/:id', validateToken,  updateRoom);
 router.get('/project/:id', validateToken, getRoomsByProject);
 
