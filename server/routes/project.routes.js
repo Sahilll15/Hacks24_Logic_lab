7b72a06d9e251f307cc6ddd99df2ccfef1a8fc2d
Slacky300 <rehman.khan.300a@gmail.com>
2024-01-31 12:47:39 +0530

    tested project controllers

--- server/routes/project.routes.js
+++ server/routes/project.routes.js
@@ -1,11 +1,11 @@
 const { createProject, deleteProject, updateProject, getProjectsByDesigner, getProjectsByOwner } = require('../controllers/project.controllers')
-
+const {validateToken} = require('../middleware/validateToken')
 const router = require('express').Router();
 
-router.post('/create', createProject);
-router.delete('/delete/:id', deleteProject);
-router.put('/update/:id', updateProject);
-router.get('/designer', getProjectsByDesigner);
-router.get('/owner', getProjectsByOwner);
+router.post('/create', validateToken, createProject);
+router.delete('/delete/:id', validateToken,  deleteProject);
+router.put('/update/:id', validateToken,  updateProject);
+router.get('/designer', validateToken, getProjectsByDesigner);
+router.get('/owner', validateToken,  getProjectsByOwner);
 
 module.exports = router;
